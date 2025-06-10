e295263037a6cd914179143afb31c962dad36725
Kacper Połuszejko <kacperpoluszejko@wp.pl>
2025-06-10 13:06:40 +0200

    lab12

---
+++ Monte_carlo_12/Monte_Carlo_12.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <fstream>
+#include <cstdlib>
+#include <vector>
+#include <ctime>
+#include <random>
+
+using namespace std;
+
+int main()
+{
+    cout<<2;
+    return 0;
+}
